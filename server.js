359b6e404d2989989702baf3b9fc21a2090ae907
J Lobos Vega <jalv.vg@gmail.com>
2019-07-19 04:56:22 -0400

    debug

--- server.js
+++ server.js
@@ -9,29 +9,38 @@ var app = express();
 
 app.use(logger("dev"));
 app.use(
-    bodyParser.urlencoded({
+  bodyParser.urlencoded({
     extended: false
   })
 );
 
 app.use(express.static(process.cwd() + "/public"));
-
 //Require set up handlebars
 var exphbs = require("express-handlebars");
-app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
 app.set("view engine", "handlebars");
-//connecting to MongoDB
-//mongoose.connect("mongodb://localhost/articleScraper");
-mongoose.connect("mongodb://localhost/articleScraper");
-var db = mongoose.connection;
 
+//connecting to MongoDB
+//mongoose.connect("mongodb://localhost/scraped_news");
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/scraper_news";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function() {
-  console.log("Connected to Mongoose! BOOYA!");
+  console.log("Connected to Mongoose!");
 });
 
+var routes = require("./controller/controller.js");
+app.use("/", routes);
 //Create localhost port
-var port = process.env.Port || 3000;
+var port = process.env.PORT || 3000;
 app.listen(port, function() {
-    console.log("Listening on PORT" + port);
+  console.log("Listening on PORT " + port);
 });
